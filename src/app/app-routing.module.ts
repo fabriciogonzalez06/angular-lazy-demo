8b28b0a505de94dcb90c5986758499e7bc025414
angel gonzalez <angelfabriciogonzalez@gmail.com>
2020-01-10 10:26:45 -0600

    demo finalizado

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'customers', loadChildren:()=> import('./customers/customers.module').then(m => m.CustomersModule) },
+  {path:'orders', loadChildren:()=> import('./orders/orders.module').then(m=>m.OrdersModule)},
+  {path:'', redirectTo:'', pathMatch:'full'}
+];
 
-@NgModule({
+
+//SIN PRE-LOAD
+/* @NgModule({
   imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+ */
+@NgModule({
+  imports: [RouterModule.forRoot(routes,{
+    enableTracing:true,
+    preloadingStrategy:PreloadAllModules //precarga  los modulos una vez cargado el main principal 
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
